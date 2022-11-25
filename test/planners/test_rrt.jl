94e6d290cd4626b1a41d10625b82b0fcc9b87f22
jinbeizame007 <jinbeizame007@gmail.com>
2022-11-25 22:36:39 +0900

    [add] test for RRT constructor

--- test/planners/test_rrt.jl
+++ test/planners/test_rrt.jl
@@ -6,3 +6,17 @@ using StaticArrays
     node = Node(position)
     @test node.position == position
 end
+
+@testset "RRT" begin
+    low = SA[0.0, 0.0]
+    high = SA[5.0, 5.0]
+
+    start = SA[1.0, 1.0]
+    goal = SA[4.0, 4.0]
+
+    @test_nowarn RRT(start, goal, low, high)
+
+    @test_throws DomainError RRT(start, goal, SA[5.0, 5.0], SA[0.0, 0.0])
+    @test_throws DomainError RRT(SA[-1.0, -1.0], goal, low, high)
+    @test_throws DomainError RRT(start, SA[-1.0, -1.0], low, high)
+end
