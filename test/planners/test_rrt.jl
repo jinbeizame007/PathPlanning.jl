c0d406091c85cc5f95d90f855e87e2b595cddb4b
jinbeizame007 <jinbeizame007@gmail.com>
2022-11-26 12:26:16 +0900

    [add] test for get_nearest_node_index()

--- test/planners/test_rrt.jl
+++ test/planners/test_rrt.jl
@@ -12,6 +12,14 @@ using StaticArrays
     @test calc_distance(node2, node1) ≈ 2.0^0.5
 end
 
+function get_default_rrt()
+    low = SA[0.0, 0.0]
+    high = SA[5.0, 5.0]
+    start = SA[1.0, 1.0]
+    goal = SA[4.0, 4.0]
+    return RRT(start, goal, low, high)
+end
+
 @testset "RRT-Init" begin
     low = SA[0.0, 0.0]
     high = SA[5.0, 5.0]
@@ -26,17 +34,36 @@ end
     @test_throws DomainError RRT(start, SA[-1.0, -1.0], low, high)
 end
 
+@testset "RRT-Sample" begin
+    rrt = get_default_rrt()
+    @test all([all(rrt.low .<= sample(rrt).position .<= rrt.high) for _ in 1:100])
+end
+
+@testset "RRT-GetNearestNodeIndex" begin
+    rrt = get_default_rrt()
+    nodes = [
+        Node(SA[0.0, 0.0]),
+        Node(SA[0.0, 5.0]),
+        Node(SA[5.0, 0.0]),
+        Node(SA[5.0, 5.0]),
+    ]
+    node = Node(SA[1.0, 1.0])
+    rrt.nodes = nodes
+
+    nearest_node_index = get_nearest_node_index(rrt, node)
+    nearest_node = rrt.nodes[nearest_node_index]
+
+    @test nearest_node_index == 1
+    @test nearest_node == nodes[1]
+end
+
 @testset "RRT-Plan" begin
     low = SA[0.0, 0.0]
     high = SA[5.0, 5.0]
-
     start = SA[1.0, 1.0]
     goal = SA[4.0, 4.0]
 
     rrt = RRT(start, goal, low, high; step_size=0.5, max_iter=10000)
-
-    @test all([all(low .<= sample(rrt).position .<= high) for _ in 1:100])
-    
     path = plan(rrt)
     @test length(path) > 0
 end
