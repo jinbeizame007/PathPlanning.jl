8533542e424d9177b833ed595b186dad683d8c28
jinbeizame007 <jinbeizame007@gmail.com>
2022-12-03 20:15:04 +0900

    [fix] move create_example_2D_env()

--- src/env/Envs.jl
+++ src/env/Envs.jl
@@ -6,7 +6,7 @@ using StaticArrays
 export RectObstacle, CircleObstacle, is_inside
 
 # from env.jl
-export Env, add_obstacle!, is_inside_any_obstacle
+export Env, add_obstacle!, is_inside_any_obstacle, create_example_2D_env
 
 include("obstacles.jl")
 include("env.jl")
