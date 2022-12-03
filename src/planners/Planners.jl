eb424eec6b36df13ab94273fe4c278f54fabc2f6
jinbeizame007 <jinbeizame007@gmail.com>
2022-12-04 00:39:41 +0900

    [add] AbstractPlanner

--- src/planners/Planners.jl
+++ src/planners/Planners.jl
@@ -3,7 +3,7 @@ module Planners
 using StaticArrays
 
 # from rrt.jl
-export Node, calc_distance, RRT, sample
+export Node, calc_distance, AbstractSampler, RRT, sample
 export get_nearest_node_index, get_extended_node
 export is_near_the_goal, plan
 
