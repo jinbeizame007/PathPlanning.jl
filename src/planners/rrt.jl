fc8732943c95cfbf2502eb3764bc0b48c4c4aa1a
jinbeizame007 <jinbeizame007@gmail.com>
2022-12-03 11:31:19 +0900

    [add] docstrings

--- src/planners/rrt.jl
+++ src/planners/rrt.jl
@@ -1,16 +1,55 @@
+"""
+    Node{N}
+
+Node configures a graph.
+
+# Fields
+- `position::SVector{N, Float64}`: position of the node in the search space
+- `parent::Union{Int64, Nothing}`: index of the parent node
+"""
 mutable struct Node{N}
     position::SVector{N, Float64}
     parent::Union{Int64, Nothing}
 end
 
-function Node(position::SVector{N, Float64}) where {N}
+"""
+    Node(position::SVector{N, Float64}) where {N}
+
+Constructor of the Node struct.
+The index of the parent node is initiated as nothing.
+
+# Arguments
+
+- `position::SVector{N, Float64}`: position of the node in the search space
+"""
+function Node(position::SVector{N, Float64})::Node{N} where {N}
     return Node{N}(position, nothing)
 end
 
+"""
+    calc_distance(node1::Node{N}, node2::Node{N})::Float64 where {N}
+
+Calculate the Euclidean distance between the two nodes in the N-dimensional space.
+"""
 function calc_distance(node1::Node{N}, node2::Node{N})::Float64 where {N}
     return sum((node1.position - node2.position).^2.0)^0.5
 end
 
+"""
+    RRT{N}
+
+A planner using RRT.
+
+# Fields
+- `start::Node{N}`: start position in the search space
+- `goal::Node{N}`: goal position in the search space
+- `low::SVector{N,Float64}`: lower bounds of the search space
+- `high::SVector{N,Float64}`: upper bounds of the search space
+- `nodes::Vector{Node{N}}`: nodes configure tje graph
+- `goal_sample_rate::Float64`: rate of sampling the goal node
+- `step_size::Float64`: maximum distance between each node
+- `max_iter::Int64`: maximum the number of the iterations
+"""
 mutable struct RRT{N}
     start::Node{N}
     goal::Node{N}
@@ -22,6 +61,29 @@ mutable struct RRT{N}
     max_iter::Int64
 end
 
+"""
+    RRT(
+        start::SVector{N,Float64},
+        goal::SVector{N,Float64},
+        low::SVector{N,Float64},
+        high::SVector{N,Float64};
+        goal_sample_rate::Float64 = 0.2,
+        step_size::Union{Float64,Nothing} = nothing,
+        max_iter::Int64 = 500
+    ) where {N}
+
+Constructor of RRT struct.
+The `step_size` is initiated as 1/20th of the distance between the start node and the start node by default.
+
+# Arguments
+- `start::Node{N}`: start position in the search space
+- `goal::Node{N}`: goal position in the search space
+- `low::SVector{N,Float64}`: lower bounds of the search space
+- `high::SVector{N,Float64}`: upper bounds of the search space
+- `goal_sample_rate::Float64`: rate of sampling the goal node
+- `step_size::Float64`: maximum distance between each node
+- `max_iter::Int64`: maximum the number of the iterations
+"""
 function RRT(
     start::SVector{N,Float64},
     goal::SVector{N,Float64},
@@ -30,7 +92,7 @@ function RRT(
     goal_sample_rate::Float64 = 0.2,
     step_size::Union{Float64,Nothing} = nothing,
     max_iter::Int64 = 500
-) where {N}
+)::RRT{N} where {N}
     if any(high .<= low)
         throw(DomainError((low, high), "The `low` ($low) should be lower than `high` ($high)."))
     end
@@ -45,22 +107,42 @@ function RRT(
 
     nodes = Vector{Node{N}}([])
     if isnothing(step_size)
-        step_size = sum((goal - start).^2.0)^0.5 / 50.0
+        step_size = sum((goal - start).^2.0)^0.5 / 20.0
     end
 
     return RRT{N}(Node(start), Node(goal), low, high, nodes, goal_sample_rate, step_size, max_iter)
 end
 
+"""
+    sample(rrt::RRT{N})::Node{N} where {N}
+
+Sample a node from the search space.
+"""
 function sample(rrt::RRT{N})::Node{N} where {N}
     return Node((@SVector rand(N)) .* (rrt.high - rrt.low) - rrt.low)
 end
 
+"""
+    get_nearest_node_index(rrt::RRT{N}, new_node::Node{N})::Int64 where {N}
+
+Return the index of the node nearest to the given node in the graph.
+"""
 function get_nearest_node_index(rrt::RRT{N}, new_node::Node{N})::Int64 where {N}
     distances = [calc_distance(node, new_node) for node in rrt.nodes]
     _, index = findmin(distances)
     return index
 end
 
+"""
+    get_extended_node(rrt::RRT{N}, nearest_node::Node{N}, new_node::Node{N})::Node{N} where {N}
+
+Create and return a node at the position from the nearest node to the new node as close as by the step size.
+
+# Arguments
+- `rrt::RRT{N}`: a planner using RRT
+- `nearest_node::Node{N}`: the node nearest to the new node in the graph
+- `new_node::Node{N}`: the node newly sampled
+"""
 function get_extended_node(rrt::RRT{N}, nearest_node::Node{N}, new_node::Node{N})::Node{N} where {N}
     delta = new_node.position - nearest_node.position
     distance = calc_distance(nearest_node, new_node)
@@ -70,11 +152,28 @@ function get_extended_node(rrt::RRT{N}, nearest_node::Node{N}, new_node::Node{N}
     return extended_node
 end
 
+"""
+    is_near_the_goal(rrt::RRT{N}, node::Node{N}) where {N}
+
+Return if given node is near the goal (distance from goal is smaller than the `rrt.step_size`.)
+
+# Arguments
+- `rrt::RRT{N}`: a planner using RRT
+- `node::Node{N}`: a node in the search space
+"""
 function is_near_the_goal(rrt::RRT{N}, node::Node{N}) where {N}
     distance_from_goal = calc_distance(rrt.goal, node)
     return distance_from_goal <= rrt.step_size
 end
 
+"""
+    extract_path(rrt::RRT{N}) where N
+
+Return a path from start node to goal node by extracting nodes.
+
+# Arguments
+- rrt::RRT{N}: a planner using RRT
+"""
 function extract_path(rrt::RRT{N}) where N
     path = Vector{Node{N}}([])
 
@@ -91,9 +190,18 @@ function extract_path(rrt::RRT{N}) where N
     return reverse(path)
 end
 
-function plan(
-    rrt::RRT{N}
-)::Vector{Node{N}} where {N}
+"""
+    plan(rrt::RRT{N})::Vector{Node{N}} where {N}
+
+Find and return a path from the start node `rrt.start` to the goal node `rrt.goal` with RRT algorithm.
+
+# Arguments
+- rrt::RRT{N}: a planner using RRT
+
+# Returns
+- `path::Vector{Node{N}}`: path (sequence of nodes) from the start node `rrt.start` to the goal node `rrt.goal`
+"""
+function plan(rrt::RRT{N})::Vector{Node{N}} where {N}
     rrt.nodes = [rrt.start]
 
     for i in 1:rrt.max_iter
