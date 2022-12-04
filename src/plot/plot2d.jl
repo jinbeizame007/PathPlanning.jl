7496c2568fae7f9ee98b4c25da16ba88ca8d5e06
jinbeizame007 <jinbeizame007@gmail.com>
2022-12-04 21:08:49 +0900

    [add] add animate() function

--- src/plot/plot2d.jl
+++ src/plot/plot2d.jl
@@ -3,10 +3,7 @@
 
 Plot each nodes as small circles.
 """
-function plot_nodes!(
-    axis::Axis,
-    nodes::Vector{Node{2}},
-)::Nothing
+function plot_nodes!(axis::Axis, nodes::Vector{Node{2}})::Nothing
     x = [node.position[1] for node in nodes]
     y = [node.position[2] for node in nodes]
     scatter!(axis, x, y)
@@ -20,10 +17,7 @@ Plot all paths as line segments.
 If you input all nodes that the planner has, you can plot all paths that the planner explorated,
 If you input nodes in a path that the planner found, you can plot a path that the planner found.
 """
-function plot_paths!(
-    axis::Axis,
-    nodes::Vector{Node{2}},
-)::Nothing
+function plot_paths!(axis::Axis, nodes::Vector{Node{2}})::Nothing
     x = Vector{Float64}([])
     y = Vector{Float64}([])
     for node in nodes[2:end]
@@ -42,10 +36,7 @@ end
 
 Plot a RectObstacle.
 """
-function plot_obstacle!(
-    axis::Axis,
-    obstacle::RectObstacle{2},
-)::Nothing
+function plot_obstacle!(axis::Axis, obstacle::RectObstacle{2})::Nothing
     corner = obstacle.center - obstacle.size./2
     size = obstacle.size
     poly!(Rect(corner[1], corner[2], size[1], size[2]))
@@ -57,10 +48,7 @@ end
 
 Plot a CircleObstacle.
 """
-function plot_obstacle!(
-    axis::Axis,
-    obstacle::CircleObstacle{2},
-)::Nothing
+function plot_obstacle!(axis::Axis, obstacle::CircleObstacle{2})::Nothing
     poly!(Circle(Point2f(obstacle.center[1], obstacle.center[2]), obstacle.radius))
     return nothing
 end
@@ -70,10 +58,7 @@ end
 
 Plot obstacles that can include RectObstacles, CircleObstacles, or both.
 """
-function plot_obstacles!(
-    axis::Axis,
-    obstacles::Vector{AbstractObstacle},
-)::Nothing
+function plot_obstacles!(axis::Axis, obstacles::Vector{AbstractObstacle})::Nothing
     for obstacle in obstacles
         plot_obstacle!(axis, obstacle)
     end
@@ -101,4 +86,48 @@ function plot(
     plot_paths!(axis, planner.nodes)
     plot_obstacles!(axis, env.obstacles)
     return figure, axis
+end
+
+"""
+    animate(
+        env::Env,
+        planner::AbstractPlanner{2};
+        resolution::Tuple{Int64,Int64}=(800,800),
+        framerate::Int64=30,
+        file_name::String="path.mp4"
+    )::Nothing
+
+Create a movie that shows the planning process.
+"""
+function animate(
+    env::Env,
+    planner::AbstractPlanner{2};
+    resolution::Tuple{Int64,Int64}=(800,800),
+    framerate::Int64=30,
+    file_name::String="path.mp4"
+)::Nothing
+    figure = Figure(backgroundcolor = :white; resolution = resolution)
+    axis = Axis(figure[1, 1])
+    plot_obstacles!(axis, env.obstacles)
+
+    # Plot start and goal node
+    scatter!(axis, [planner.start.position[1]], [planner.start.position[2]])
+    scatter!(axis, [planner.goal.position[1]], [planner.goal.position[2]])
+
+    num_frames = length(planner.nodes)
+    frame_indices = 2:num_frames
+
+    record(figure, file_name, frame_indices; framerate=framerate) do frame_index
+        node = planner.nodes[frame_index]
+        parent_node = planner.nodes[node.parent]
+        scatter!(axis, [node.position[1]], [node.position[2]]; color=:dodgerblue)
+        linesegments!(
+            axis,
+            [parent_node.position[1], node.position[1]],
+            [parent_node.position[2], node.position[2]];
+            color=:dodgerblue
+        )
+    end
+
+    return nothing
 end
