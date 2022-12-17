fb2cabf803d13f74e75d710144f516105fd1ddd1
jinbeizame007 <jinbeizame007@gmail.com>
2022-12-17 22:38:29 +0900

    [fix] refactor rrt_star.jl

--- src/planners/rrt_star.jl
+++ src/planners/rrt_star.jl
@@ -13,6 +13,8 @@ A planner using RRT*.
 - `step_size::Float64`: maximum distance between each node
 - `max_iter::Int64`: maximum the number of the iterations
 - `is_approved::Union{Function, Nothing}`: a function that returns if the node can be added the graph
+- `enable_logging::Bool`: whether to log (is_goaled, RRTStar instance) for each steps in planning
+- `logs::Vector{Dict{String, Any}}`: (is_goaled, RRTStar instance) for each steps in planning
 """
 mutable struct RRTStar{N} <: AbstractRRT{N}
     start::Node{N}
@@ -24,6 +26,8 @@ mutable struct RRTStar{N} <: AbstractRRT{N}
     step_size::Float64
     max_iter::Int64
     is_approved::Union{Function, Nothing}
+    enable_logging::Bool
+    logs::Vector{Dict{String, Any}}
 end
 
 """
@@ -50,6 +54,7 @@ The `step_size` is initiated as 1/20th of the distance between the start node an
 - `step_size::Float64`: maximum distance between each node
 - `max_iter::Int64`: maximum the number of the iterations
 - `is_approved::Union{Function, Nothing}`: a function that returns if the node can be added the graph
+- `enable_logging::Bool`: whether to log (is_goaled, RRTStar instance) for each steps in planning
 """
 function RRTStar(
     start::SVector{N,Float64},
@@ -60,6 +65,7 @@ function RRTStar(
     step_size::Union{Float64,Nothing} = nothing,
     max_iter::Int64 = 500,
     is_approved::Union{Function, Nothing} = nothing,
+    enable_logging::Bool = false
 )::RRTStar{N} where {N}
     if any(high .<= low)
         throw(DomainError((low, high), "The `low` ($low) should be lower than `high` ($high)."))
@@ -78,7 +84,34 @@ function RRTStar(
         step_size = sum((goal - start).^2.0)^0.5 / 20.0
     end
 
-    return RRTStar{N}(Node(start), Node(goal), low, high, nodes, goal_sample_rate, step_size, max_iter, is_approved)
+    logs = Vector{Dict{String, Any}}([])
+    return RRTStar{N}(Node(start), Node(goal), low, high, nodes, goal_sample_rate, step_size, max_iter, is_approved, enable_logging, logs)
+end
+
+"""
+    add_node!(rrt_star::RRTStar{N}, new_node::Node{N}, parent_node_index::Int64)::Nothing where {N}
+
+Add new node with the parent index and the cost to the graph.
+The cost of the new node is the cost of the parent node and the distance between the parent node and the new node.
+"""
+function add_node!(rrt_star::RRTStar{N}, new_node::Node{N}, parent_node_index::Int64)::Nothing where {N}
+    parent_node = rrt_star.nodes[parent_node_index]
+    new_node.parent = parent_node_index
+    new_node.cost = parent_node.cost + calc_distance(parent_node, new_node)
+    push!(rrt_star.nodes, new_node)
+    return nothing
+end
+
+"""
+    get_shortest_parent_node_index(rrt::RRTStar{N}, new_node::Node{N})::Int64 where {N}
+
+Get the parent node that minimizes the length between the start node and the new node.
+"""
+function get_shortest_parent_node_index(rrt::RRTStar{N}, new_node::Node{N})::Int64 where {N}
+    tol = 1e-8
+    distances = [calc_distance(node, new_node) <= rrt.step_size + tol ? node.cost : 1e10 + calc_distance(node, new_node) for node in rrt.nodes]
+    _, index = findmin(distances)
+    return index
 end
 
 """
@@ -125,6 +158,12 @@ function plan(rrt_star::RRTStar{N})::Vector{Node{N}} where {N}
     rrt_star.nodes = [rrt_star.start]
     is_goaled = false
 
+    logs = Vector{Dict{String, Any}}([])
+    if rrt_star.enable_logging
+        log = Dict("is_goaled" => is_goaled, "planner" => deepcopy(rrt_star))
+        push!(logs, log)
+    end
+
     for i in 1:rrt_star.max_iter
         # Sample a node
         new_node = if rand() < rrt_star.goal_sample_rate
@@ -137,6 +176,7 @@ function plan(rrt_star::RRTStar{N})::Vector{Node{N}} where {N}
         nearest_node_index = get_nearest_node_index(rrt_star, new_node)
         nearest_node = rrt_star.nodes[nearest_node_index]
 
+        # Get a node extended from the nearest node
         distance_from_nearest_node = calc_distance(nearest_node, new_node)
         if rrt_star.step_size < distance_from_nearest_node
             new_node = get_extended_node(rrt_star, nearest_node, new_node)
@@ -148,21 +188,27 @@ function plan(rrt_star::RRTStar{N})::Vector{Node{N}} where {N}
         end
 
         # Add the new node to the graph
-        new_node.parent = nearest_node_index
-        push!(rrt_star.nodes, new_node)
+        parent_node_index = get_shortest_parent_node_index(rrt_star, new_node)
+        add_node!(rrt_star, new_node, parent_node_index)
         new_node_index = length(rrt_star.nodes)
 
+        # Add goal node to the graph if the new node is near the goal node
         if !is_goaled && is_near_the_goal(rrt_star, new_node)
             new_node_index = length(rrt_star.nodes)
-            rrt_star.goal.parent = new_node_index
+            add_node!(rrt_star, rrt_star.goal, new_node_index)
             is_goaled = true
         end
 
-        num_nodes = length(rrt_star.nodes)
-        distance_threshold = 50.0 * (log(num_nodes) / num_nodes)^(1/N)
-        near_node_indices = get_near_node_indices(rrt_star, new_node, distance_threshold)
+        # Rewire near nodes
+        near_node_indices = get_near_node_indices(rrt_star, new_node, rrt_star.step_size)
         rewire_near_nodes!(rrt_star, near_node_indices, new_node_index)
+
+        if rrt_star.enable_logging
+            log = Dict("is_goaled" => is_goaled, "planner" => deepcopy(rrt_star))
+            push!(logs, log)
+        end
     end
 
+    rrt_star.logs = logs
     return extract_path(rrt_star)
 end
