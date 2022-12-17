49a08b46675fd38b6d8facb52f878f8b63e48439
jinbeizame007 <jinbeizame007@gmail.com>
2022-12-17 13:50:39 +0900

    [add] inclde rrt_star.jl

--- src/planners/Planners.jl
+++ src/planners/Planners.jl
@@ -5,8 +5,12 @@ using StaticArrays
 # from rrt.jl
 export Node, calc_distance, AbstractPlanner, RRT, sample
 export get_nearest_node_index, get_extended_node
-export is_near_the_goal, plan
+export is_near_the_goal, extract_path, plan
+
+# from rrt_star.jl
+export RRTStar, plan
 
 include("rrt.jl")
+include("rrt_star.jl")
 
 end # module
