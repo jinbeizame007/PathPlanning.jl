afd5cdd3783095fa0d3611f6263fae628fc97578
jinbeizame007 <jinbeizame007@gmail.com>
2023-03-17 23:51:22 +0900

    [fix] remove export calc_distance() from Envs

--- src/env/Envs.jl
+++ src/env/Envs.jl
@@ -3,7 +3,7 @@ module Envs
 using StaticArrays
 
 # from obstacles.jl
-export AbstractObstacle, RectObstacle, CircleObstacle, is_inside, calc_distance
+export AbstractObstacle, RectObstacle, CircleObstacle, is_inside
 
 # from env.jl
 export Env, add_obstacle!, is_inside_any_obstacle, create_example_2D_env, create_example_2D_env_stomp
